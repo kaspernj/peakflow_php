fae467bb200572b6c5b4276d00859fae5cfe25ca
kjabtion <kj@abtion.com>
2019-01-16 16:54:43 +0100

    Worked a lot more on the reporter

--- src/PeakFlow/Reporter.php
+++ src/PeakFlow/Reporter.php
@@ -3,7 +3,61 @@
 namespace PeakFlow;
 
 class Reporter {
-  function __construct() {
-    print("Hello world\n");
+  function __construct($args) {
+    $this->authToken = $args["authToken"];
+  }
+
+  function getUrl() {
+    if (!array_key_exists("SERVER_PORT", $_SERVER)) {
+      return null;
+    }
+
+    $url = "http";
+
+    if ($_SERVER["SERVER_PORT"] == 443) {
+      $url .= "s";
+    }
+
+    $url .= "//";
+    $url .= $_SERVER["HTTP_HOST"];
+    $url .= $_SERVER["REQUEST_URI"];
+
+    return $url;
+  }
+
+  function getUserAgent() {
+    if (array_key_exists("HTTP_USER_AGENT", $_SERVER)) {
+      return $_SERVER["HTTP_USER_AGENT"];
+    }
+  }
+
+  function reportException($exception) {
+    $this->report(array(
+      "auth_token" => $this->authToken,
+      "error" => array(
+        "backtrace" => $exception->getTrace(),
+        "error_class" => get_class($exception),
+        "message" => $exception->getMessage(),
+        "url" => $this->getUrl(),
+        "user_agent" => $this->getUserAgent()
+      )
+    ));
+  }
+
+  function report($data) {
+    $url = "https://www.peakflow.io/errors/reports";
+    $options = array(
+      "http" => array(
+        "header" => "Content-Type: application/x-www-form-urlencoded\r\n",
+        "method" => "POST",
+        "content" => http_build_query($data)
+      )
+    );
+    $context = stream_context_create($options);
+    $result = file_get_contents($url, false, $context);
+
+    if ($result === false) {
+      throw new Exception("Couldn't report the error");
+    }
   }
 }
--- tests/PeakFlow/ReporterTest.php
+++ tests/PeakFlow/ReporterTest.php
@@ -4,6 +4,12 @@ use PHPUnit\Framework\TestCase;
 
 class PeakFlowReporterTest extends TestCase {
   function testReporting() {
-    $reporter = new PeakFlow/Reporter();
+    $reporter = new PeakFlow\Reporter(array("authToken" => "TEST_AUTH_TOKEN"));
+
+    try {
+      throw new RuntimeException("Test");
+    } catch(Exception $e) {
+      $reporter->reportException($e);
+    }
   }
 }
