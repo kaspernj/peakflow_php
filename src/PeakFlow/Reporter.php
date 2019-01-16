d3552612e30d6b3fd3ff21ef5a1ba13ab2dc8351
kjabtion <kj@abtion.com>
2019-01-16 16:06:16 +0100

    More work

---
+++ src/PeakFlow/Reporter.php
@@ -0,0 +1,9 @@
+<?php
+
+namespace PeakFlow;
+
+class Reporter {
+  function __construct() {
+    print("Hello world\n");
+  }
+}
---
+++ tests/PeakFlow/ReporterTest.php
@@ -0,0 +1,9 @@
+<?php
+
+use PHPUnit\Framework\TestCase;
+
+class PeakFlowReporterTest extends TestCase {
+  function testReporting() {
+    $reporter = new PeakFlow/Reporter();
+  }
+}
